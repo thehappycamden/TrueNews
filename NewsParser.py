384330c9be79f96ac2427cfd424203d355ea86bd
thehappycamden <lego76666@gmail.com>
2019-06-05 20:13:55 +0000

    Second Commit. Can use NewsParser.py to convert scraped_articles.json to plaintext

--- NewsParser.py
+++ NewsParser.py
@@ -1,1 +1,25 @@
-#!/home/ubuntu/environment/NeuralNet/bin/ python3
+#!/home/ubuntu/environment/NeuralNet/bin/python3
+
+import json
+
+text = ""
+
+with open("scraped_articles.json") as json_file:
+    data = json.load(json_file)
+    papers = data["newspapers"]
+    for name, data in papers.items():
+        link = data['link']
+        text_current = f"-----\n\nSITE: {link}"
+        if 'rss' in data:
+            rss = data['rss']
+            text_current += f"RSS: {rss}"
+        text_current += "\n\n"
+        for article in data['articles']:
+            title = article['title']
+            published = article['published']
+            content = article['text']
+            text_current += f'-----\n\nTITLE: {title}\nPUBLISHED: {published}\nTEXT: {content}\n\n'
+        text += text_current
+
+with open("plain_articles.txt", "w") as plaintext:
+    plaintext.write(text)
